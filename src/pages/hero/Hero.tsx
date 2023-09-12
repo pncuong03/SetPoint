52a4e93fdafbd2d10159f70b483dca6e70d10b85
pncuong03 <phamngoccuonggl3022002@gmail.com>
2023-09-12 17:20:05 +0700

    update-7

--- src/pages/hero/Hero.tsx
+++ src/pages/hero/Hero.tsx
@@ -6,10 +6,10 @@ export const Hero = () => {
       <div className="flex flex-col px-28 my-3 items-center justify-center ">
         <div className="flex flex-col items-center gap-[120px] lg:flex-row lg:items-center lg:justify-around">
           <div className="flex flex-col gap-[20px] items-center text-center w-[350px] lg:items-start lg:w-[120%] md:w-[100%]">
-            <h1 className="text-4xl md:text-11xl lg:text-12xl 2xl:text-45xl  lg:text-start font-inter font-bold m-0">
+            <h1 className="text-4xl md:text-11xl lg:text-12xl 2xl:text-29xl  lg:text-start font-inter font-bold m-0">
               Welcome to the Treaty community! Experience bursts of luck and jackpots. Are you the chosen one to receive the genie's wish?
             </h1>
-            <p className="text-4xl md:text-11xl lg:text-12xl 2xl:text-45xl lg:text-start font-inter font-normal m-0">
+            <p className="text-4xl md:text-11xl lg:text-12xl 2xl:text-29xl lg:text-start font-inter font-normal m-0">
               First Memecoin GameFi project on Sui Network
             </p>
             <div className="text-4xl lg:text-12xl text-white lg:items-start text-center relative">
